342b47adc270564be924b44236ca49ccc3c5cce0
Giovani Coelho <giovanicoelho2004@hotmail.com>
2023-04-25 15:17:39 -0300

    feat: board generate

--- src/main/java/com/giovani/model/Tabuleiro.java
+++ src/main/java/com/giovani/model/Tabuleiro.java
@@ -1,4 +1,42 @@
 package com.giovani.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Tabuleiro {
+  private int x;
+  private int y;
+  private int mines;
+
+  private final List<Campo> fields = new ArrayList<>();
+
+  public Tabuleiro(int x, int y, int mines) {
+    this.x = x;
+    this.y = y;
+    this.mines = mines;
+
+    generateField();
+    associateNeighbor();
+    raffleMines();
+  }
+
+  private void generateField() {
+    for (int line = 0; line < x; line++) {
+      for (int column = 0; column < y; column++) {
+        fields.add( new Campo ( x, column ));
+      }
+    }
+  }
+
+  private void associateNeighbor() {
+    for(Campo c1 : fields) {
+      for(Campo c2 : fields) {
+        c1.addNeighbor(c2);
+      }
+    }
+  }
+
+  private void raffleMines() {
+    
+  }
 }
